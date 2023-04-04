260cea981c2332b53913398338d9b1f18578e51b
Dao Khoi Nguyen <nguyendk.on@gmail.com>
2023-04-05 01:14:24 +0700

    chore: update source code

---
+++ src/Desktop.AI.App/Desktop.AI.App/Program.cs
@@ -0,0 +1,67 @@
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+
+using OpenAI.Net;
+
+namespace Desktop.AI.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+
+            builder.Services.AddOpenAIServices(o =>
+            {
+                o.ApiKey = builder.Configuration["OpenAI:ApiKey"];
+            });
+
+            builder.Services.AddElectron();
+            builder.WebHost.UseElectron(args);
+
+
+            if (HybridSupport.IsElectronActive)
+            {
+                // Open the Electron-Window here
+                Task.Run(async () => {
+                    var window = await Electron
+                    .WindowManager
+                    .CreateWindowAsync(new BrowserWindowOptions()
+                        { 
+                            AutoHideMenuBar = true,
+                        }
+                        );
+
+                    window.OnClosed += () => {
+                        Electron.App.Quit();
+                    };
+                });
+            }
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
